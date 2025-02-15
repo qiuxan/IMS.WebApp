3f07330c4376d37650e8883b1e0f13b211a8a5cc
qiuxan <qiuxan@gmail.com>
2025-02-15 20:13:43 +1100

    Create TaskCase Plugin Pending Inventory Interface

---
+++ IMS.Usecase/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMS.CoreBusiness;
+
+namespace IMS.Usecase.Inventories;
+public class ViewInventoriesByNameUseCase
+{
+    private readonly IInventoryRepository inventoryRepository;
+
+    public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+    {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    // It is to present the list of inventories so the function return type is IEnumerable of Inventory
+    public async Task<IEnumerable<Inventory>> ExcuteAsync(string name = "")
+    {
+        return await inventoryRepository.GetInventoriesByName(name);
+
+    }
+}
